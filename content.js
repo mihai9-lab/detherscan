94f7ddadab95102633990477e81f62c4018e1e7a
mihai9-lab <mihailo.sky@gmail.com>
2023-03-13 15:49:41 +0100

    Open .deth.net in new tab.

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ if(isContract){
   const link = window.location.href;
   const dethLink = link.replace('.io','.deth.net');
   const button = `
-    <a id="deth-link" href="${dethLink}" class="btn btn-sm btn-primary">
+    <a id="deth-link" href="${dethLink}" class="btn btn-sm btn-primary" target="_blank">
       <img src="https://etherscan.deth.net/favicon.svg" alt="deth.net"/>
       Deth
     </a>`;
